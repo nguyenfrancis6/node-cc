365e256a8d3b8387abc35fedc0b188717f110f56
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-08-26 15:34:35 -0700

    .

---
+++ global.js
@@ -0,0 +1,15 @@
+// in browser, the global object 'window' is implied
+
+//global object
+
+// global.setTimeout(() => {
+//   console.log('in the timeout')
+//   clearInterval(int)
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log('in the interval')
+// }, 1000)
+
+console.log(__dirname);
+console.log(__filename);
