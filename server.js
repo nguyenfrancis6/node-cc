776ecbdf5b135e8f0089d3240e084c75e03d6580
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-08-27 21:14:17 -0700

    how to make responses, sending different html files based off route, reading html files, status codes

--- server.js
+++ server.js
@@ -1,9 +1,49 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log('request made');
+  console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  // send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+
+  // res.write("<p>Hello</p>");
+  // res.write('<p style="color: red; font-weight: 800;">Hello again</p>');
+  // res.end();
 });
 
-server.listen(3000, 'localhost', () => {
-  console.log('listening for request on port 3000')
-})
+server.listen(3000, "localhost", () => {
+  console.log("listening for request on port 3000");
+});
