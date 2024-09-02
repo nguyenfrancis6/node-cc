8e6755f0fc5474ab6fda2b295678544a3120aac7
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-09-02 15:01:45 -0700

    using view engines (ejs) to render instead of reading html files

--- app.js
+++ app.js
@@ -3,27 +3,37 @@ const express = require("express");
 // express app
 const app = express();
 
-//listen for requests, automatically infers localhost
+// register view engine
+app.set('view engine', 'ejs');
+
+// listen for requests, automatically infers localhost
 app.listen(3000);
 
 app.get("/", (req, res) => {
  // res.send("<p>home page</p>"); // express figures out header and status code for us
-  res.sendFile('./views/index.html', { root: __dirname }); //express uses absolute path, can set root to current directory using __dirname
+  // res.sendFile('./views/index.html', { root: __dirname }); //express uses absolute path, can set root to current directory using __dirname
+  res.render('index');
 });
 
 app.get("/about", (req, res) => {
  // res.send("<p>about page</p>");
- res.sendFile('./views/about.html', { root: __dirname });
+//  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about');
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+// // redirects
+// app.get('/about-us', (req, res) => {
+//   res.redirect('/about');
+// })
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create');
 })
 
 // 404 page, matter orders for app.use, if all the .get methods fail, .use will be used for every request after
 // in this .use case, express doesnt know status code so we set ourselves
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  // res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404');
 })
 
