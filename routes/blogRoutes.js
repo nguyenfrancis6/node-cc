88497e0f53e982217e9ac9cbae3e6be8bc7561f4
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-09-08 21:00:42 -0700

    used express router to separate blog routes

---
+++ routes/blogRoutes.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const Blog = require('../models/blog')
+const router = express.Router();
+
+// blog routes
+router.get('/', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'All Blogs', blogs: result })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+router.post('/', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+// getting create works when above the /blogs/:id routes because it thinks 'create' 
+// is a potential id, causing the create file to not render
+router.get('/create', (req, res) => {
+  res.render('create', { title: 'Create a new Blog'});
+})
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details'});
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+module.exports = router;
