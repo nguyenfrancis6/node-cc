f20f183497f44614d9989a4a831191abed5e26c9
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-09-08 21:21:59 -0700

    added controller to follow MVC design and moved blog view files to its own folder

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,56 +1,11 @@
 const express = require('express');
-const Blog = require('../models/blog')
+const blogController = require('../controllers/blogController')
 const router = express.Router();
 
-// blog routes
-router.get('/', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then((result) => {
-      res.render('index', { title: 'All Blogs', blogs: result })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-})
-
-router.post('/', (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog.save()
-    .then((result) => {
-      res.redirect('/blogs');
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-})
-
-// getting create works when above the /blogs/:id routes because it thinks 'create' 
-// is a potential id, causing the create file to not render
-router.get('/create', (req, res) => {
-  res.render('create', { title: 'Create a new Blog'});
-})
-
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then(result => {
-      res.render('details', { blog: result, title: 'Blog Details'});
-    })
-    .catch(err => {
-      console.log(err);
-    })
-})
-
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then(result => {
-      res.json({ redirect: '/blogs' });
-    })
-    .catch(err => {
-      console.log(err);
-    })
-})
+router.get('/', blogController.blog_index)
+router.post('/', blogController.blog_create_post)
+router.get('/create', blogController.blog_create_get) // getting create doesn't work below the /blogs/:id routes because it thinks 'create' is a potential id, causing the create file to not render
+router.get('/:id', blogController.blog_details)
+router.delete('/:id', blogController.blog_delete)
 
 module.exports = router;
